3e109e2f99b7a72d431063911441375aa2d50173
Riley Mathews <rileymathews80@gmail.com>
2018-08-04 08:35:36 -0500

    added views to render

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './nav/NavBar';
+import ApplicationViews from './ApplicationViews';
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <NavBar />
+        <ApplicationViews />
       </React.Fragment>
     );
   }
