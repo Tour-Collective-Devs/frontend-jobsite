9940d9d6b271d73fd418e45b4116bc89deb56c54
JacobAndrewSmith92 <jacobandrewsmith92@gmail.com>
2018-08-04 08:48:08 -0500

    Linked homepage and app.js

---
+++ src/home/landingPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class landingPage extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Tour Collective Careers</h1>
+            </div>
+        )
+    }
+}
