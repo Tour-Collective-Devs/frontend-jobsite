8fa4c8a5443d2badbfb23b5aae7aaebcbd261fd8
Riley Mathews <rileymathews80@gmail.com>
2018-08-04 09:01:18 -0500

    added provider file

---
+++ src/Provider.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+/*
+    module: context provider
+    author: riley mathews
+    purpose: to hold state at a top level in the application and pass that down to any component that would need to use it
+*/
+
+class Provider extends Component {
+
+
+    render() {
+        return (
+            <React.Fragment>
+
+            </React.Fragment>
+        )
+    }
+}
+
+export default Provider
