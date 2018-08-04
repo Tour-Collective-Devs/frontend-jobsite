732bf72016789e440fa80118782fa956bcd50950
Riley Mathews <rileymathews80@gmail.com>
2018-08-04 09:11:26 -0500

    added context provider

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 import NavBar from './nav/NavBar';
 import ApplicationViews from './ApplicationViews';
+import { Provider } from './Provider';
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <NavBar />
-        <ApplicationViews />
+        <Provider>
+          <NavBar />
+          <ApplicationViews />
+        </Provider>
       </React.Fragment>
     );
   }
--- src/Provider.js
+++ src/Provider.js
@@ -6,16 +6,39 @@ import React, { Component } from 'react'
     purpose: to hold state at a top level in the application and pass that down to any component that would need to use it
 */
 
-class Provider extends Component {
+export const Context = React.createContext()
 
+export class Provider extends Component {
 
+    /*
+    The initial state of the data provider should include
+    default values for any top-level component that will
+    need the data. In this case, PoliticianList is my only
+    top-level component. It is not a child of any other
+    component.
+    */
+    state = {
+        testUser = {
+            first_name: 'john',
+            last_name: 'doe',
+            email: 'email@website.com'
+        }
+    }
+
+    /*
+        This component will not render any DOM element itself.
+        Rather it becomes a virtual wrapper around any component
+        that wants to serve as the data provider for its children.
+    */
     render() {
         return (
-            <React.Fragment>
+            <Context.Provider value={{
+                state: this.state,
+            }}>
+                {this.props.children}
+            </Context.Provider>
 
-            </React.Fragment>
         )
     }
 }
 
-export default Provider
