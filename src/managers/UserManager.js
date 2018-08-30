0be018b9bd1c2f6c611075e925e2a602565a997d
Riley Mathews <rileymathews80@gmail.com>
2018-08-29 22:45:20 -0500

    added code to run if registration was not sucsessful

--- src/managers/UserManager.js
+++ src/managers/UserManager.js
@@ -12,13 +12,28 @@ const UserManager = Object.create(null, {
     // method for registering a new user
     register: {
         value: function (userToRegister) {
-            
+            // pass user object onto api manager
             APIManager.registerNewUser(userToRegister)
                 .then(r => r.json())
                 .then(response => {
-                    const token = response.key
-                    this.setState({ userToken: token })
-                    localStorage.setItem('token', token)
+                    // check to see if response is the key, indicated a sucsessful registration
+                    if (response.key) {
+                        // get the token
+                        const token = response.key
+                        // set token in state and local storage to be used in future requests to api
+                        this.setState({ userToken: token })
+                        localStorage.setItem('token', token)
+                    
+                    // else logic to run if response did not contain an auth token
+                    } else {
+                        // build up error string
+                        let errorString = ""
+                        // add to error string for each item in the error response object from django
+                        for (const key in response) {
+                            errorString += `${key}: ${response[key]} \n`
+                        }
+                        alert(errorString)
+                    }
                 })
         }
     }
